36547cc7ad973577509140943a2a3e1b4213ca5f
ampersand <ampersand.dev@gmail.com>
2021-04-28 15:38:00 +0900

    参加者集約がほとんど完成 細かいロジックは必要になり次第つくる

---
+++ serverSide/src/domain/participant/participant/participantName.ts
@@ -0,0 +1,21 @@
+import { ValueObject } from '../../../shared/domain/ValueObject';
+
+interface ParticipantNameProps {
+  participantName: string;
+}
+
+export class ParticipantName extends ValueObject<ParticipantNameProps> {
+
+  // 名前、苗字最低でも１文字ずつとして、最低２文字入力されいること
+  public static minimumLength = 2;
+
+  private constructor(props: ParticipantNameProps) {
+    super(props);
+  }
+  static create(props: ParticipantNameProps): ParticipantName {
+    if (props.participantName.length < this.minimumLength) {
+      throw new Error('名前をフルネームで入力してください。');
+    }
+    return new ParticipantName(props);
+  }
+}
---
+++ serverSide/src/domain/participant/team/team.ts
@@ -0,0 +1,61 @@
+import { Entity } from '../../../shared/domain/Entity';
+import { TeamId } from './teamId';
+import { UniqueEntityID } from '../../../shared/domain/UniqueEntityID';
+import { TeamName } from './teamName';
+import { Pair } from '../pair/pair';
+
+interface TeamProps {
+  teamName: TeamName;
+  pairs: Pair[];
+  upperLimit: number;
+  lowerLimit: number;
+}
+
+export class Team extends Entity<TeamProps> {
+
+  get id(): TeamId {
+    return TeamId.create(this._id);
+  }
+
+  private constructor(props: TeamProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+  static create(props: TeamProps, id?: UniqueEntityID): Team {
+    // todo 重複チェックのドメインサービスをつくる
+    //
+    let participantCount = 0;
+    props.pairs.map((pair: Pair) => {
+      participantCount += pair.props.participants.length;
+    });
+    console.log(`このチームの参加者は${participantCount}人です`);
+    if (participantCount < props.lowerLimit) {
+      throw new Error(
+        `チームに所属する参加者の人数が足りません。チームの下限は${props.lowerLimit}名です。`,
+      );
+    }
+
+    if (props.pairs.length > props.upperLimit) {
+      throw new Error(
+        `チームに所属する参加者の人数が多すぎます。チームの上限は${props.upperLimit}名です。`,
+      );
+    }
+    return new Team(props, id);
+  }
+
+  canAdd(): boolean {
+    // todo ロジックを書く
+    return true;
+  }
+  canRemove(): boolean {
+    // todo ロジックを書く
+    return true;
+  }
+  addPair(): Team {
+    // todo ロジックを書く
+    return Team.create(this.props);
+  }
+  removePair(): Team {
+    // todo ロジックを書く
+    return Team.create(this.props);
+  }
+}
