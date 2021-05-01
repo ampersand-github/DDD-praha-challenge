5fd2d698482f43a9fb8e49a1941c3530374303a2
ampersand <ampersand.dev@gmail.com>
2021-05-01 20:56:52 +0900

    main用

--- serverSide/src/domain/participant/participantAR.ts
+++ serverSide/src/domain/participant/participantAR.ts
@@ -1,29 +1,13 @@
 export class ParticipantAR {
-  // todo private or protected
-  // todo readonly or static
+  // Todo 集約ルートを作る
   private readonly participantId: string;
-  private readonly name: string;
-  private readonly mailAddress: string;
-  private readonly enrolledStatus: string;
-  // todo ペアとかチームとか
-  public constructor(props: {
-    participantId: string;
-    name: string;
-    mailAddress: string;
-    enrolledStatus: string;
-  }) {
-    const { participantId, name, mailAddress, enrolledStatus } = props;
+  public constructor(props: { participantId: string }) {
+    const { participantId } = props;
     this.participantId = participantId;
-    this.name = name;
-    this.mailAddress = mailAddress;
-    this.enrolledStatus = enrolledStatus;
   }
-
   public getAllProperties() {
     return {
       participantId: this.participantId,
-      name: this.name,
-      mailAddress: this.enrolledStatus,
     };
   }
 }
