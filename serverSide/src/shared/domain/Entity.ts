4a6c4fb5633cc9daf22d6fe37981543b0b87776c
ampersand <ampersand.dev@gmail.com>
2021-05-05 14:04:44 +0900

    public get id()と public get values():Tを追加 props型をprotectedへ

--- serverSide/src/shared/domain/Entity.ts
+++ serverSide/src/shared/domain/Entity.ts
@@ -5,13 +5,22 @@ const isEntity = (v: any): v is Entity<any> => {
 };
 
 export abstract class Entity<T> {
-  protected readonly _id: UniqueEntityID;
-  // テスト
-  protected readonly props: T;
+  private readonly _id: UniqueEntityID;
+  // 型をprotectedにして、継承先クラスからしかアクセスできないようにする。
+  // (クラス外からアクセスできないようにする)
+  protected props: T;
 
   constructor(props: T, id?: UniqueEntityID) {
     this._id = id ? id : new UniqueEntityID();
     this.props = props;
+  }
+
+  public get id(): UniqueEntityID {
+    return this._id;
+  }
+
+  public get values():T {
+    return this.props;
   }
 
   public equals(object?: Entity<T>): boolean {
