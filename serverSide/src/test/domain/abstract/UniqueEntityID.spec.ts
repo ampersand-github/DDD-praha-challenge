189290314a253baf0d57e24b6c206b9082b120d4
ampersand <ampersand.dev@gmail.com>
2021-05-17 17:47:46 +0900

    数値しかuuidにできなかったのを英数小文字okに修正

--- serverSide/src/test/domain/abstract/UniqueEntityID.spec.ts
+++ serverSide/src/test/domain/abstract/UniqueEntityID.spec.ts
@@ -7,7 +7,7 @@ describe('UniqueEntityID', () => {
     }).toThrowError('aaaはuuidの型として正しくありません。');
   });
   test('uuidとして正しい形式の場合はうまくいく', () => {
-    const uuid = '99999999-9999-9999-9999-999999999999';
+    const uuid = '99999999-9999-9999-9999-99999999999a';
     const id = new UniqueEntityID(uuid);
     expect(id.toValue()).toBe(uuid);
   });
