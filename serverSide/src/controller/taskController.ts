baad6a0d054cbc7a5d58c427ec01135d0606f45a
ampersand <ampersand.dev@gmail.com>
2021-06-01 02:10:29 +0900

    コントローラ修正

---
+++ serverSide/src/controller/taskController.ts
@@ -0,0 +1,91 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TaskRepository } from '../infra/db/repository/taskRepository';
+import { whenEmptyOutputError } from '../shared/controller/whenEmptyOutputError';
+import { TaskDTO } from '../dto/usecase/task/taskDTO';
+import { FindAllTaskUsecase } from '../usecase/task/findAllTaskUsecase';
+import { FindOneTaskUsecase } from '../usecase/task/findOneTaskUsecase';
+import { DeleteTaskUsecase } from '../usecase/task/deleteTaskUsecase';
+import { CreateTaskUsecase } from '../usecase/task/createTaskUsecase';
+import { UpdateTaskUsecase } from '../usecase/task/updateTaskUsecase';
+
+//
+// 動かすときはデータを初期化してから動かすこと
+// npm run db:reset:withData
+//
+@Controller('task')
+export class TaskController {
+  // curl -X GET http://localhost:3000/task
+  @Get()
+  public async findAll(): Promise<TaskDTO[]> {
+    const repo = new TaskRepository();
+    const usecase = new FindAllTaskUsecase(repo);
+    return await usecase.do();
+  }
+  // curl -X GET http://localhost:3000/task/99999999-9999-aaaa-aaaa-999999999999
+  @Get('/:id')
+  public async findOne(@Param('id') id: string): Promise<TaskDTO> {
+    const repo = new TaskRepository();
+    const usecase = new FindOneTaskUsecase(repo);
+    const data = { id: id };
+    return await usecase.do(data);
+  }
+
+  // curl -X DELETE http://localhost:3000/task/99999999-9999-aaaa-aaaa-999999999999
+  @Delete('/:id')
+  public async delete(@Param('id') id: string) {
+    const repo = new TaskRepository();
+    const usecase = new DeleteTaskUsecase(repo);
+    const data = { id: id };
+    return await usecase.do(data);
+  }
+
+  // curl -X POST http://localhost:3000/task -d 'name=ダミータスク2' -d 'description=ダミーの説明2' -d 'group=設計'
+  @Post()
+  public async create(
+    @Body('name') name: string,
+    @Body('description') description: string,
+    @Body('group') group: string,
+  ) {
+    whenEmptyOutputError(name);
+    whenEmptyOutputError(description);
+    whenEmptyOutputError(group);
+    const data = {
+      name: name,
+      description: description,
+      group: group,
+    };
+    const repo = new TaskRepository();
+    const usecase = new CreateTaskUsecase(repo);
+    return await usecase.do(data);
+  }
+
+  // curl -X PATCH http://localhost:3000/task/99999999-9999-aaaa-aaaa-999999999999 -d 'newNo=1' -d 'newName=HTTPヘッダを理解しない' -d 'newDescription=新しい説明文' -d 'newGroup=テスト'
+  // curl -X PATCH http://localhost:3000/task/99999999-9999-aaaa-aaaa-999999999999 -d 'newName=HTTPヘッダを理解しなくもない'
+  @Patch('/:id')
+  public async update(
+    @Param('id') taskId: string,
+    @Body('newNo') newNo: string,
+    @Body('newName') newName: string,
+    @Body('newDescription') newDescription: string,
+    @Body('newGroup') newGroup: string,
+  ) {
+    const data = {
+      taskId: taskId,
+      newNo: newNo === undefined ? undefined : parseInt(newNo),
+      newName: newName,
+      newDescription: newDescription,
+      newGroup: newGroup,
+    };
+    const repo = new TaskRepository();
+    const usecase = new UpdateTaskUsecase(repo);
+    return await usecase.do(data);
+  }
+}
---
+++ serverSide/src/controller/taskGroupController.ts
@@ -0,0 +1,54 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { TaskGroupDTO } from '../dto/usecase/task/taskGroupDTO';
+import { whenEmptyOutputError } from '../shared/controller/whenEmptyOutputError';
+import { TaskGroupRepository } from '../infra/db/repository/taskGroupRepository';
+import { FindAllTaskGroupUsecase } from '../usecase/taskGroup/FindAllTaskGroupUsecase';
+import { CreateTaskGroupUsecase } from '../usecase/taskGroup/CreateTaskGroupUsecase';
+import { DeleteTaskGroupUsecase } from '../usecase/taskGroup/DeleteTaskGroupUsecase';
+
+//
+// 動かすときはデータを初期化してから動かすこと
+// npm run db:reset:withData
+//
+
+@Controller('taskGroup')
+export class TaskGroupController {
+  // curl -X GET http://localhost:3000/taskGroup
+  @Get()
+  public async findAll(): Promise<TaskGroupDTO[]> {
+    const repo = new TaskGroupRepository();
+    const usecase = new FindAllTaskGroupUsecase(repo);
+    return await usecase.do();
+  }
+
+  // curl -X DELETE http://localhost:3000/taskGroup/99999999-9999-aaaa-aaaa-999999999999
+  @Delete('/:id')
+  public async delete(@Param('id') id: string) {
+    const repo = new TaskGroupRepository();
+    const usecase = new DeleteTaskGroupUsecase(repo);
+    const data = { id: id };
+    return await usecase.do(data);
+  }
+
+  // curl -X POST http://localhost:3000/taskGroup -d 'name=ここに新しいタスク名'
+  @Post()
+  public async create(@Body('name') name: string) {
+    whenEmptyOutputError(name);
+    const repo = new TaskGroupRepository();
+    const usecase = new CreateTaskGroupUsecase(repo);
+    const data = { name: name };
+    return await usecase.do(data);
+  }
+  /*
+  // curl -X PATCH http://localhost:3000/task/99999999-9999-aaaa-aaaa-999999999999 -d 'newName=ここに更に新しいタスク名'
+  @Patch('/:id')
+  public async update(
+    @Param('id') taskId: string,
+    @Body('newName') newName: string,
+  ) {
+    const repo = new TaskGroupRepository();
+    const usecase = new UpdateTaskGroupUsecase(repo);
+    return await usecase.do(newName);
+  }
+ */
+}
