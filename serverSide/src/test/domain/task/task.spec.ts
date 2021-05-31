da582facf5bbcb9b78b19a5e81968ebef6af510a
ampersand <ampersand.dev@gmail.com>
2021-06-01 02:17:10 +0900

    テストをしっかりやる

--- serverSide/src/test/domain/task/task.spec.ts
+++ serverSide/src/test/domain/task/task.spec.ts
@@ -1,17 +1,8 @@
-import { TaskGroup, TaskGroupEnum } from '../../../domain/task/taskGroup';
 import { Task } from '../../../domain/task/task';
+import { task1 } from '../../../test-dummy/domain/taskDummyData';
 
 describe('Task', (): void => {
-  const data = {
-    no: 1,
-    name: 'よく使うHTTPヘッダを理解する',
-    description:
-      'HTTPは様々な情報をやりとりしますが、その実態は「ヘッダー」で挙動を変化させて、情報を「ボディ」で送信する、非常にシンプルな作りです',
-    group: TaskGroup.create({ taskGroup: TaskGroupEnum.webBasic }),
-  };
-
   test('オブジェクトが生成できること', () => {
-    const actual = Task.create(data);
-    expect(actual).toBeInstanceOf(Task);
+    expect(task1).toBeInstanceOf(Task);
   });
 });
--- serverSide/src/test/domain/task/taskGroup.spec.ts
+++ serverSide/src/test/domain/task/taskGroup.spec.ts
@@ -1,10 +1,22 @@
 import { TaskGroup, TaskGroupEnum } from '../../../domain/task/taskGroup';
 
 describe('TaskGroup', (): void => {
-  const taskGroup = { taskGroup: TaskGroupEnum.webBasic };
-
-  test('オブジェクトが生成できること', () => {
+  test('TaskGroupEnum型でオブジェクトが生成できること', () => {
+    const taskGroup = { taskGroup: TaskGroupEnum.webBasic };
     const actual = TaskGroup.create(taskGroup);
     expect(actual).toBeInstanceOf(TaskGroup);
+  });
+
+  test('string型でオブジェクトが生成できること', () => {
+    const taskGroup = { taskGroup: '設計' };
+    const actual = TaskGroup.create(taskGroup);
+    expect(actual).toBeInstanceOf(TaskGroup);
+  });
+  test('TaskGroupEnumに存在しない値なら失敗する', () => {
+    const taskGroup = { taskGroup: 'ああああ' };
+
+    expect(() => {
+      TaskGroup.create(taskGroup);
+    }).toThrowError('タスクグループ名が不正です。');
   });
 });
