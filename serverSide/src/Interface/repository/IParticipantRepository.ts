157646bc15fd44212bbad94cbd486a7d7b8a412c
ampersand <ampersand.dev@gmail.com>
2021-06-03 16:27:26 +0900

    削除

--- serverSide/src/Interface/repository/IParticipantRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ParticipantDTO } from '../../dto/usecase/participant/participant/participantDataDTO';
-
-export interface IParticipantRepository {
-  getAllParticipant(): Promise<ParticipantDTO[]>;
-  getOne(id: number): any;
-  create(text: string): any;
-  update(id: number, title: string): any;
-  delete(id: number): any;
-}
