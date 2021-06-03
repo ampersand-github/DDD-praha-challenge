619076e624474e9b15b179b7107f659f2a8c4195
ampersand <ampersand.dev@gmail.com>
2021-06-03 15:54:19 +0900

    ファイルパス修正

--- serverSide/src/domain/participant/team/smallestTeamDomainService.ts
+++
@@ -1,9 +0,0 @@
-import { UniqueEntityID } from '../../../shared/domain/UniqueEntityID';
-
-export function smallestTeamDomainService(): UniqueEntityID {
-  /*
-    todo
-     DBから参加者が最小のチームのidを取得する
-     */
-  return new UniqueEntityID();
-}
