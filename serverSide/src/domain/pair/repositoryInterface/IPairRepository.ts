69394db9a558c53dad7c69dbbbfa8249cdc53ae4
ampersand <ampersand.dev@gmail.com>
2021-06-16 19:06:37 +0900

    updateを分割

--- serverSide/src/domain/pair/repositoryInterface/IPairRepository.ts
+++ serverSide/src/domain/pair/repositoryInterface/IPairRepository.ts
@@ -5,7 +5,6 @@ export interface IPairRepository {
   findAll(): Promise<Pair[]>;
   findOne(pairName: string): Promise<Pair>;
   create(pair: Pair): Promise<Pair>;
-  removeParticipant(pairName: string, participants: Participant[]): Promise<Pair>;
-  addParticipant(pairName: string, participants: Participant[]): Promise<Pair>;
+  update(pair: Pair): Promise<Pair>;
   delete(pair: Pair): Promise<number>; // numberは削除件数
 }
