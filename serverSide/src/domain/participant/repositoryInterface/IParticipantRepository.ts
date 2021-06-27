ef8780d331ca0fe5229d50faf9c50a8ebeb4a1fb
ampersand <ampersand.dev@gmail.com>
2021-06-28 00:19:34 +0900

    IParticipantRepository

--- serverSide/src/domain/participant/repositoryInterface/IParticipantRepository.ts
+++ serverSide/src/domain/participant/repositoryInterface/IParticipantRepository.ts
@@ -1,20 +1,12 @@
-import { PersonalInfo } from '../personalInfo';
 import { Participant } from '../participant';
-import { ParticipantHavingTasks } from '../participantHavingTasks';
-import { EnrolledStatus } from '../enrolledStatus';
 import { Task } from '../../task/task';
 
 export interface IParticipantRepository {
-  findAll(): Promise<PersonalInfo[]>;
-  // todo 実装するときにparticipantIdをParticipantに入れ込むことを忘れずに
+  findAll(): Promise<Participant[]>;
   findOne(participantId: string): Promise<Participant | null>;
   isExistMailAddress(mailAddress: string): Promise<boolean>;
   create(participant: Participant): Promise<Participant>;
-  updatePersonalInfo(participant: Participant): Promise<PersonalInfo>;
-  updateParticipantHavingTasks(participant: Participant): Promise<ParticipantHavingTasks>;
-  updateEnrolledStatus(participant: Participant): Promise<EnrolledStatus>;
-  //
-  // todo DBの参加者の一部の項目を個人情報テーブルに分離すること
+  update(participant: Participant): Promise<Participant>;
   delete(participant: Participant): Promise<number>; // numberは削除件数
-  deleteUserHavingTasksByTask(task: Task): Promise<number>; // numberは削除件数
+  deleteParticipantHavingTaskByTask(task: Task): Promise<number>;
 }
