a0eed6e3ed07dc25e5d1ea3953f6db932f77434c
ampersand <ampersand.dev@gmail.com>
2021-06-27 22:42:53 +0900

    participantHavingTasks.tsの分割に伴うダミーデータの修正

--- serverSide/src/testUtil/dummy/dummyParticipantHavingTasks.ts
+++ serverSide/src/testUtil/dummy/dummyParticipantHavingTasks.ts
@@ -1,20 +1,107 @@
 import { dummyTask1, dummyTask2, dummyTask3 } from './dummyTask';
 import { ProgressStatus, ProgressStatusEnum } from '../../domain/participant/progressStatus';
-import { Task } from '../../domain/task/task';
-import { ParticipantHavingTasks } from '../../domain/participant/participantHavingTasks';
+import { ParticipantHavingTask } from '../../domain/participant/participantHavingTask';
+import { ParticipantHavingTaskCollection } from '../../domain/participant/participantHavingTaskCollection';
+import { UniqueEntityID } from '../../domain/shared/UniqueEntityID';
 
-const complete = ProgressStatusEnum.complete;
-const readyForReview = ProgressStatusEnum.readyForReview;
-const notStarted = ProgressStatusEnum.notStarted;
+const complete = ProgressStatus.create({ progressStatus: ProgressStatusEnum.complete });
+const readyForReview = ProgressStatus.create({
+  progressStatus: ProgressStatusEnum.readyForReview,
+});
+const notStarted = ProgressStatus.create({ progressStatus: ProgressStatusEnum.notStarted });
 
-export const dummyStatusAndTasksData = new Map<Task, ProgressStatus>([
-  [dummyTask1, ProgressStatus.create({ progressStatus: readyForReview })],
-  [dummyTask2, ProgressStatus.create({ progressStatus: complete })],
-  [dummyTask3, ProgressStatus.create({ progressStatus: notStarted })],
-]);
-export const dummyParticipantHavingTasksData1 = {
-  statusAndTasks: dummyStatusAndTasksData,
+export const dummyParticipantHavingTask1 = ParticipantHavingTask.create({
+  task: dummyTask1,
+  progressStatus: complete,
+});
+export const dummyParticipantHavingTask2 = ParticipantHavingTask.create({
+  task: dummyTask2,
+  progressStatus: readyForReview,
+});
+export const dummyParticipantHavingTask3 = ParticipantHavingTask.create({
+  task: dummyTask3,
+  progressStatus: notStarted,
+});
+
+export const dummyParticipantHavingTaskCollectionData1 = {
+  participantHavingTaskCollection: [
+    dummyParticipantHavingTask1,
+    dummyParticipantHavingTask2,
+    dummyParticipantHavingTask3,
+  ],
 };
-export const dummyParticipantHavingTasks1 = ParticipantHavingTasks.create(
-  dummyParticipantHavingTasksData1,
-);
+
+export const dummyParticipantHavingTasks1 = ParticipantHavingTaskCollection.create({
+  participantHavingTaskCollection: [
+    ParticipantHavingTask.create(
+      {
+        task: dummyTask1,
+        progressStatus: complete,
+      },
+      new UniqueEntityID('99999999-aaad-zzzz-bbbb-999999999999'),
+    ),
+
+    ParticipantHavingTask.create({
+      task: dummyTask2,
+      progressStatus: readyForReview,
+    }),
+    ParticipantHavingTask.create({
+      task: dummyTask3,
+      progressStatus: notStarted,
+    }),
+  ],
+});
+
+export const dummyParticipantHavingTasks2 = ParticipantHavingTaskCollection.create({
+  participantHavingTaskCollection: [
+    ParticipantHavingTask.create({
+      task: dummyTask1,
+      progressStatus: complete,
+    }),
+    ParticipantHavingTask.create({
+      task: dummyTask2,
+      progressStatus: readyForReview,
+    }),
+    ParticipantHavingTask.create({
+      task: dummyTask3,
+      progressStatus: notStarted,
+    }),
+  ],
+});
+
+export const dummyParticipantHavingTasks3 = ParticipantHavingTaskCollection.create({
+  participantHavingTaskCollection: [
+    ParticipantHavingTask.create({
+      task: dummyTask1,
+      progressStatus: complete,
+    }),
+    ParticipantHavingTask.create({
+      task: dummyTask2,
+      progressStatus: readyForReview,
+    }),
+    ParticipantHavingTask.create({
+      task: dummyTask3,
+      progressStatus: notStarted,
+    }),
+  ],
+});
+export const dummyParticipantHavingTasks1update = ParticipantHavingTaskCollection.create({
+  participantHavingTaskCollection: [
+    ParticipantHavingTask.create(
+      {
+        task: dummyTask1,
+        progressStatus: complete,
+      },
+      new UniqueEntityID('99999999-aaad-zzzz-bbbb-999999999999'),
+    ),
+
+    ParticipantHavingTask.create({
+      task: dummyTask2,
+      progressStatus: complete,
+    }),
+    ParticipantHavingTask.create({
+      task: dummyTask3,
+      progressStatus: readyForReview,
+    }),
+  ],
+});
