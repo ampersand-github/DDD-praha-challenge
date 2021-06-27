b8b93ea1f6b14858b413e3ac05230fb43fb1cff0
ampersand <ampersand.dev@gmail.com>
2021-06-28 00:03:00 +0900

    participantHavingTasks.tsの分割に伴う修正および、受け取る引数を参加者(集約ルート)に統一

--- serverSide/src/test/usecase/participant/DTO/participantHavingTasksDTO.spec.ts
+++ serverSide/src/test/usecase/participant/DTO/participantHavingTasksDTO.spec.ts
@@ -1,15 +1,15 @@
-import { ParticipantHavingTasksDTO } from '../../../../usecase/participant/DTO/participantHavingTasksDTO';
 import { TaskDTO } from '../../../../usecase/task/DTO/taskDTO';
-import { dummyParticipantHavingTasks1 } from '../../../../testUtil/dummy/dummyParticipantHavingTasks';
 import { dummyTask1, dummyTask2, dummyTask3 } from '../../../../testUtil/dummy/dummyTask';
+import { ParticipantHavingTaskCollectionDTO } from '../../../../usecase/participant/DTO/participantHavingTasksDTO';
+import { dummyParticipant1 } from '../../../../testUtil/dummy/dummyPerticipant';
 
 describe('ParticipantHavingTasksDTO', (): void => {
   describe('constructor', (): void => {
     test('[正常]返り値が取得できる', () => {
-      const actual = new ParticipantHavingTasksDTO(dummyParticipantHavingTasks1);
-      expect(actual.participantHavingTasks).toStrictEqual([
-        [new TaskDTO(dummyTask1), 'レビュー待ち'],
-        [new TaskDTO(dummyTask2), '完了'],
+      const actual = new ParticipantHavingTaskCollectionDTO(dummyParticipant1);
+      expect(actual.tasks).toStrictEqual([
+        [new TaskDTO(dummyTask1), '完了'],
+        [new TaskDTO(dummyTask2), 'レビュー待ち'],
         [new TaskDTO(dummyTask3), '未着手'],
       ]);
     });
