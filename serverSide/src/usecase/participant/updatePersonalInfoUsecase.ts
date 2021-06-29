29c995bcb78edf008b3345cb1c7f4b5e94dfdc11
ampersand <ampersand.dev@gmail.com>
2021-06-29 22:11:03 +0900

    テスト通らなかったので修正

--- serverSide/src/usecase/participant/updatePersonalInfoUsecase.ts
+++ serverSide/src/usecase/participant/updatePersonalInfoUsecase.ts
@@ -1,6 +1,7 @@
 import { IParticipantRepository } from '../../domain/participant/repositoryInterface/IParticipantRepository';
 import { PersonalInfo } from '../../domain/participant/personalInfo';
 import { DisallowDuplicateMailAddressService } from '../../domain/participant/service/disallowDuplicateMailaddressService';
+import { Participant } from '../../domain/participant/participant';
 
 interface UpdatePersonalInfoUsecaseProps {
   participantId: string;
@@ -20,26 +21,25 @@ export class UpdatePersonalInfoUsecase {
     this.service = service;
   }
 
-  public async do(props: UpdatePersonalInfoUsecaseProps): Promise<PersonalInfo> {
+  public async do(props: UpdatePersonalInfoUsecaseProps): Promise<Participant> {
     // idから参加者ドメインオブジェクトを取得
-    let participant = await this.repo.findOne(props.participantId);
+    const participant = await this.repo.findOne(props.participantId);
     if (!participant) {
       throw new Error('この参加者は存在しません。');
     }
-
     //
     // メールアドレスのアップデート
     if (props.shouldUpdateMailAddress) {
       await this.service.do({ mailAddress: props.shouldUpdateMailAddress });
-      participant = participant.changeMailAddress(props.shouldUpdateMailAddress);
+      participant.changeMailAddress(props.shouldUpdateMailAddress);
     }
 
     // 名前のアップデート
     if (props.shouldUpdateName) {
-      participant = participant.changeParticipantName(props.shouldUpdateName);
+      participant.changeParticipantName(props.shouldUpdateName);
     }
 
     // DBに登録
-    return await this.repo.updatePersonalInfo(participant);
+    return await this.repo.update(participant);
   }
 }
