354cf50e93e8516b976cd45cc474dd2d8fdf1a1d
ampersand <ampersand.dev@gmail.com>
2021-07-10 17:46:35 +0900

    TaskGroupDeleteDomainService

--- serverSide/src/test/domain/task/taskDeleteDomainService.spec.ts
+++ serverSide/src/test/domain/task/taskDeleteDomainService.spec.ts
@@ -1,36 +1,58 @@
-import { InMemoryTaskRepository } from '../../../infra/db/inMemory/inMemoryTaskRepository';
-import { InMemoryParticipantRepository } from '../../../infra/db/inMemory/inMemoryParticipantRepository';
+import { TaskGroupDeleteDomainService } from '../../../domain/taskGroup/taskGroupDeleteDomainService';
+import { prismaClient } from '../../../util/prisma/prismaClient';
+import { ParticipantRepository } from '../../../infra/db/repository/participantRepository';
+import { TaskRepository } from '../../../infra/db/repository/taskRepository';
+import { truncateAllTable } from '../../../testUtil/reposiotry/truncateAllTable';
+import { dummyTask1, dummyTask2, dummyTask3 } from '../../../testUtil/dummy/dummyTask';
+import { dummyParticipant1, dummyParticipant3 } from '../../../testUtil/dummy/dummyPerticipant';
 import { TaskDeleteDomainService } from '../../../domain/task/taskDeleteDomainService';
 
-describe('TaskDeleteDomainService', () => {
-  const participantRepository = new InMemoryParticipantRepository();
-  const taskRepository = new InMemoryTaskRepository();
+describe('TaskGroupDeleteDomainService', () => {
+  const prisma = prismaClient;
+  const participantRepository = new ParticipantRepository(prisma);
+  const taskRepository = new TaskRepository(prisma);
   const taskDeleteDomainService = new TaskDeleteDomainService(
     taskRepository,
     participantRepository,
   );
 
-  beforeEach(() => {
+  beforeAll(() => {
+    truncateAllTable();
+  });
+
+  beforeEach(async () => {
+    await prismaClient.personalInfo.deleteMany();
+    await prismaClient.participantHavingTask.deleteMany();
+    await prismaClient.task.deleteMany();
+    await prismaClient.participant.deleteMany();
+    await taskRepository.create(dummyTask1);
+    await taskRepository.create(dummyTask2);
+    await taskRepository.create(dummyTask3);
+    await participantRepository.create(dummyParticipant1);
+    await participantRepository.create(dummyParticipant3);
+  });
+
+  afterAll(async () => {
+    await prismaClient.$disconnect();
+  });
+  beforeEach(async () => {
     jest.clearAllMocks();
   });
 
-  // todo リポジトリをモックしてのテストが面倒くさいというか、モックしたテストが意味ないので、リポジトリ実装時にテストする
   describe('do', (): void => {
     test('[正常]削除できる', async () => {
-      // データ作成
-      // 結果確認
-    });
-    test('[異常]引数のタスクグループが存在しないタスクグループ', async () => {
-      // データ作成
-      // 結果確認
-    });
-    test('[正常]削除対象のタスクグループに紐づくタスクが存在しない場合でも正常に動作する', async () => {
-      // データ作成
-      // 結果確認
-    });
-    test('[正常]削除対象のタスクに紐づく参加者保有課題が存在しない場合でも正常に動作する', async () => {
-      // データ作成
-      // 結果確認
+      const beforeHavingTaskCount = await prisma.participantHavingTask.count();
+      expect(beforeHavingTaskCount).toBe(6);
+      const beforeTaskCount = await prisma.task.count();
+      expect(beforeTaskCount).toBe(3);
+
+      const taskId = dummyTask1.id.toValue();
+      await taskDeleteDomainService.do({ taskId: taskId });
+
+      const afterHavingTaskCount = await prisma.participantHavingTask.count();
+      expect(afterHavingTaskCount).toBe(4);
+      const afterTaskCount = await prisma.task.count();
+      expect(afterTaskCount).toBe(2);
     });
   });
 });
