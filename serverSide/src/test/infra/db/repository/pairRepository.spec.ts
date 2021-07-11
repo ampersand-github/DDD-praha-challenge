1af995c128d423c56b25675fc2994d9f169ee664
ampersand <ampersand.dev@gmail.com>
2021-07-11 13:44:44 +0900

    beforeEach作成

--- serverSide/src/test/infra/db/repository/pairRepository.spec.ts
+++ serverSide/src/test/infra/db/repository/pairRepository.spec.ts
@@ -1,19 +1,42 @@
 import { truncateAllTable } from '../../../../testUtil/reposiotry/truncateAllTable';
 import { prismaClient } from '../../../../util/prisma/prismaClient';
 import { PairRepository } from '../../../../infra/db/repository/pairRepository';
+import {
+  dummyParticipant1,
+  dummyParticipant3,
+  dummyParticipant4,
+  dummyParticipant5,
+  dummyParticipant6,
+  dummyParticipant7,
+} from '../../../../testUtil/dummy/dummyPerticipant';
+import { ParticipantRepository } from '../../../../infra/db/repository/participantRepository';
+import { dummyTask1, dummyTask2, dummyTask3 } from '../../../../testUtil/dummy/dummyTask';
+import { TaskRepository } from '../../../../infra/db/repository/taskRepository';
 
 describe('PairRepository', (): void => {
-  const repo = new PairRepository(prismaClient);
+  const prisma = prismaClient;
+  const taskRepository = new TaskRepository(prisma);
+  const participantRepository = new ParticipantRepository(prisma);
+  const pairRepository = new PairRepository(prisma);
 
   beforeAll(() => {
     truncateAllTable();
   });
 
   beforeEach(async () => {
-    // await prismaClient.participantHavingTask.deleteMany();
-    // await prismaClient.task.deleteMany(); // truncateと同じ
-    // await repo.create(dummyTask1);
-    //  await repo.create(dummyTask2);
+    await prismaClient.personalInfo.deleteMany();
+    await prismaClient.participantHavingTask.deleteMany();
+    await prismaClient.task.deleteMany();
+    await prismaClient.participant.deleteMany();
+    await taskRepository.create(dummyTask1);
+    await taskRepository.create(dummyTask2);
+    await taskRepository.create(dummyTask3);
+    await participantRepository.create(dummyParticipant1);
+    await participantRepository.create(dummyParticipant3);
+    await participantRepository.create(dummyParticipant4);
+    await participantRepository.create(dummyParticipant5);
+    await participantRepository.create(dummyParticipant6);
+    await participantRepository.create(dummyParticipant7);
   });
 
   afterAll(async () => {
