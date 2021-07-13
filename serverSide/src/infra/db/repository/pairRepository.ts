4b6609f2c6c1f94a684fa704072a48e20de33799
ampersand <ampersand.dev@gmail.com>
2021-07-13 13:49:18 +0900

    delete IGAI ZISSOU

--- serverSide/src/infra/db/repository/pairRepository.ts
+++ serverSide/src/infra/db/repository/pairRepository.ts
@@ -1,115 +1,89 @@
-import { PrismaClient, Task as PrismaTaskProps } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { IPairRepository } from '../../../domain/pair/repositoryInterface/IPairRepository';
 import { Pair } from '../../../domain/pair/pair';
+import { IConverter } from './shared/converter';
+import { prismaClient } from '../../../util/prisma/prismaClient';
+import { dummyWithdrawal } from '../../../testUtil/dummy/dummyEnrolledStatus';
 
 export class PairRepository implements IPairRepository {
   private readonly prismaClient: PrismaClient;
+  private readonly converter: IConverter;
 
-  public constructor(prismaClient: PrismaClient) {
+  public constructor(prismaClient: PrismaClient, converter: IConverter) {
     this.prismaClient = prismaClient;
+    this.converter = converter;
   }
 
   public async findAll(): Promise<Pair[]> {
-    return Promise.resolve([]);
+    const findManyPair = await this.prismaClient.pair.findMany({
+      orderBy: {
+        pairName: 'asc',
+      },
+      include: {
+        participants: {
+          include: { personalInfo: true, participantHavingTask: true },
+        },
+      },
+    });
+    return Promise.all(findManyPair.map((one) => this.converter.toPair(one)));
   }
 
-  public async findOne(pairName: string): Promise<Pair> {
-    return Promise.resolve(undefined);
+  public async findOne(pairId: string): Promise<Pair> {
+    const result = await prismaClient.pair.findUnique({
+      where: { pairId: pairId },
+      include: {
+        participants: {
+          include: { personalInfo: true, participantHavingTask: true },
+        },
+      },
+    });
+    return this.converter.toPair(result);
   }
 
   public async create(pair: Pair): Promise<Pair> {
-    return Promise.resolve(undefined);
+    const participants = pair.participants.map((one) => {
+      return {
+        participantId: one.id.toValue(),
+      };
+    });
+    await this.prismaClient.pair.create({
+      data: {
+        pairId: pair.id.toValue(),
+        pairName: pair.pairName,
+        participants: {
+          connect: participants,
+        },
+      },
+      include: { participants: true },
+    });
+    return await this.findOne(pair.id.toValue());
   }
 
   public async update(pair: Pair): Promise<Pair> {
-    return Promise.resolve(undefined);
+    await prismaClient.participant.updateMany({
+      where: { pairName: pair.pairName },
+      data: {
+        pairName: null,
+      },
+    });
+
+    await Promise.all(
+      pair.participants.map(async (one) => {
+        await prismaClient.participant.update({
+          where: { participantId: one.id.toValue() },
+          data: {
+            pair: {
+              connect: { pairId: pair.id.toValue() },
+              update: { pairName: pair.pairName },
+            },
+          },
+        });
+      }),
+    );
+    return await this.findOne(pair.id.toValue());
   }
 
   public async delete(pair: Pair): Promise<number> {
     return Promise.resolve(0);
   }
-
-  /*
-
-  public async findAll(): Promise<Task[]> {
-    const findMany = await this.prismaClient.task.findMany({
-      orderBy: {
-        taskNo: 'asc',
-      },
-    });
-    return findMany.map((one) => TaskRepository.convertTo(one));
-  }
- */
-
-  /*
-  public async findOne(id: string): Promise<Task> {
-    const taskData = await this.prismaClient.task.findUnique({
-      where: {
-        taskId: id,
-      },
-    });
-    return TaskRepository.convertTo(taskData);
-  }
- */
-
-  /*
-  public async findByTaskGroup(taskGroup: TaskGroup): Promise<Task[]> {
-    const taskData = await this.prismaClient.task.findMany({
-      where: {
-        taskGroupName: taskGroup.taskGroup,
-      },
-    });
-    return taskData.map((one) => {
-      return TaskRepository.convertTo(one);
-    });
-  }
- */
-
-  /*
-  public async create(task: Task): Promise<Task> {
-    const prismaTask = await this.prismaClient.task.create({
-      data: {
-        taskId: task.id.toValue(),
-        taskNo: task.no,
-        taskName: task.name,
-        description: task.description,
-        taskGroupName: task.group,
-      },
-    });
-    return TaskRepository.convertTo(prismaTask);
-  }
- */
-
-  /*
-  public async delete(task: Task): Promise<number> {
-    const result1 = await this.prismaClient.participantHavingTask.deleteMany({
-      where: {
-        taskId: task.id.toValue(),
-      },
-    });
-    const result2 = await this.prismaClient.task.deleteMany({
-      where: {
-        taskId: task.id.toValue(),
-      },
-    });
-    return result1.count + result2.count;
-  }
- */
-
-  /*
-  public async update(task: Task): Promise<Task> {
-    const prismaTask = await this.prismaClient.task.update({
-      where: {
-        taskId: task.id.toValue(),
-      },
-      data: {
-        taskNo: task.no,
-        taskName: task.name,
-        description: task.description,
-        taskGroupName: task.group,
-      },
-    });
-    return TaskRepository.convertTo(prismaTask);
-  }
- */
 }
--- serverSide/src/test/infra/db/repository/pairRepository.spec.ts
+++ serverSide/src/test/infra/db/repository/pairRepository.spec.ts
@@ -12,16 +12,19 @@ import {
 import { ParticipantRepository } from '../../../../infra/db/repository/participantRepository';
 import { dummyTask1, dummyTask2, dummyTask3 } from '../../../../testUtil/dummy/dummyTask';
 import { TaskRepository } from '../../../../infra/db/repository/taskRepository';
-import { dummyPair1 } from '../../../../testUtil/dummy/dummyPair';
+import { dummyPair2, dummyPair3 } from '../../../../testUtil/dummy/dummyPair';
+import { Converter } from '../../../../infra/db/repository/shared/converter';
+import clone from 'clone';
 
 describe('PairRepository', (): void => {
   const prisma = prismaClient;
-  const taskRepository = new TaskRepository(prisma);
-  const participantRepository = new ParticipantRepository(prisma);
-  const pairRepository = new PairRepository(prisma);
+  const converter: Converter = new Converter(prisma);
+  const taskRepository = new TaskRepository(prisma, converter);
+  const participantRepository = new ParticipantRepository(prisma, converter);
+  const pairRepository = new PairRepository(prisma, converter);
 
-  beforeAll(() => {
-    truncateAllTable();
+  beforeAll(async () => {
+    await truncateAllTable();
   });
 
   beforeEach(async () => {
@@ -29,16 +32,20 @@ describe('PairRepository', (): void => {
     await prismaClient.participantHavingTask.deleteMany();
     await prismaClient.task.deleteMany();
     await prismaClient.participant.deleteMany();
+    await prismaClient.pair.deleteMany();
+    console.log(10);
     await taskRepository.create(dummyTask1);
     await taskRepository.create(dummyTask2);
     await taskRepository.create(dummyTask3);
+    console.log(20);
     await participantRepository.create(dummyParticipant1);
     await participantRepository.create(dummyParticipant3);
     await participantRepository.create(dummyParticipant4);
     await participantRepository.create(dummyParticipant5);
     await participantRepository.create(dummyParticipant6);
     await participantRepository.create(dummyParticipant7);
-    await pairRepository.create(dummyPair1);
+    console.log(30);
+    await pairRepository.create(dummyPair2);
   });
 
   afterAll(async () => {
@@ -47,30 +54,74 @@ describe('PairRepository', (): void => {
 
   describe('create()', () => {
     test('[正常]作成できる', async () => {
-      // データ作成
-      // 結果確認
+      // before
+      expect(await prisma.pair.count()).toBe(1);
+      // after
+      const result = await pairRepository.create(dummyPair3);
+      expect(await prisma.pair.count()).toBe(2);
+      await expect(result).toStrictEqual(dummyPair3);
     });
   });
 
   describe('findAll()', () => {
     test('[正常]取得できる', async () => {
-      // データ作成
-      // 結果確認
+      expect(await prisma.pair.count()).toBe(1);
+      await pairRepository.create(dummyPair3);
+      const actual = await pairRepository.findAll();
+      expect(actual.length).toBe(2);
+      expect(await prisma.pair.count()).toBe(2);
     });
   });
+
   describe('findOne()', () => {
     test('[正常]取得できる', async () => {
-      // データ作成
-      // 結果確認
+      const actual = await pairRepository.findOne(dummyPair2.id.toValue());
+      expect(actual.equals(dummyPair2)).toBe(true);
     });
   });
 
   describe('update()', () => {
     test('[正常]更新できる', async () => {
-      // データ作成
-      // 結果確認
+      await pairRepository.create(dummyPair3);
+
+      // createしたペアに所属する参加者のペア名がdummyPair3のペア名
+      const beforeParticipants = await prisma.participant.findMany({
+        where: { pairName: dummyPair3.pairName },
+      });
+      await Promise.all(
+        beforeParticipants.map((one) => expect(one.pairName).toEqual(dummyPair3.pairName)),
+      );
+
+      // 更新できているか確かめるために参加者の削除と更新をする
+      const pair = clone(dummyPair3);
+      pair.removeParticipant(dummyParticipant5);
+      pair.addParticipant(dummyParticipant1);
+
+      // データの更新と等価比較
+      const result = await pairRepository.update(pair);
+      expect(result.equals(pair)).toBe(true);
+
+      // 更新後テーブルの参加者のペアの確認
+      const afterParticipants = await prisma.participant.findMany();
+      const p5 = await afterParticipants.find(
+        (one) => dummyParticipant5.id.toValue() === one.participantId,
+      );
+      const p1 = await afterParticipants.find(
+        (one) => dummyParticipant1.id.toValue() === one.participantId,
+      );
+      const p6 = await afterParticipants.find(
+        (one) => dummyParticipant6.id.toValue() === one.participantId,
+      );
+      const p7 = await afterParticipants.find(
+        (one) => dummyParticipant7.id.toValue() === one.participantId,
+      );
+      expect(p5.pairName).toBe(null);
+      expect(p1.pairName).toBe('c');
+      expect(p6.pairName).toBe('c');
+      expect(p7.pairName).toBe('c');
     });
   });
+
   describe('delete()', () => {
     test('[正常]削除できる', async () => {
       // データ作成
