3b17a05f7c8de75a7391f08afbb613f1b41c9e26
ampersand <ampersand.dev@gmail.com>
2021-07-20 00:37:33 +0900

    ToRecessStatusUsecaseをメモリー上のテストからリポジトリのDBのテストに修正

--- serverSide/src/usecase/participant/toRecessStatusUsecase.ts
+++ serverSide/src/usecase/participant/toRecessStatusUsecase.ts
@@ -2,25 +2,43 @@ import { IParticipantRepository } from '../../domain/participant/repositoryInter
 import { EnrolledStatusDTO } from './DTO/enrolledStatusDTO';
 import { EnrolledStatusEnum } from '../../domain/participant/enrolledStatus';
 import { Participant } from '../../domain/participant/participant';
+import { IPairRepository } from '../../domain/pair/repositoryInterface/IPairRepository';
+import { RemoveParticipantInPairUsecase } from '../pair/removeParticipantInPairUsecase';
 
 interface ToRecessStatusProps {
   participantId: string;
 }
 
 export class ToRecessStatusUsecase {
-  private readonly repo: IParticipantRepository;
+  private readonly participantRepository: IParticipantRepository;
+  private readonly pairRepository: IPairRepository;
+  private readonly removeParticipantInPairUsecase: RemoveParticipantInPairUsecase;
 
-  public constructor(repository: IParticipantRepository) {
-    this.repo = repository;
+  public constructor(
+    participantRepository: IParticipantRepository,
+    pairRepository: IPairRepository,
+    removeParticipantInPairUsecase: RemoveParticipantInPairUsecase,
+  ) {
+    this.participantRepository = participantRepository;
+    this.pairRepository = pairRepository;
+    this.removeParticipantInPairUsecase = removeParticipantInPairUsecase;
   }
 
   public async do(props: ToRecessStatusProps): Promise<EnrolledStatusDTO> {
-    const currentParticipant: Participant = await this.repo.findOne(props.participantId);
+    const currentParticipant: Participant = await this.participantRepository.findOne(
+      props.participantId,
+    );
+
     currentParticipant.changeEnrolledStatus(EnrolledStatusEnum.recess);
-    //
-    // todo [高] ペアの削除のドメインサービスをここにいれる
-    //
-    const result = await this.repo.update(currentParticipant);
+    const result = await this.participantRepository.update(currentParticipant);
+
+    // ペアの削除のドメインサービス
+    const targetPair = await this.pairRepository.findOneByParticipant(currentParticipant);
+    await this.removeParticipantInPairUsecase.do({
+      pairId: targetPair.id.toValue(),
+      removeParticipant: currentParticipant,
+    });
+
     return new EnrolledStatusDTO(result);
   }
 }
