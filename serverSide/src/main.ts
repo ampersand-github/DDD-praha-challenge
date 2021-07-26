b3477ae44449adb4aee35d50ffd30a85a85fe8a9
ampersand <ampersand.dev@gmail.com>
2021-07-26 18:20:21 +0900

    npm run で開発環境用変数と本番環境用変数を切り替える

--- serverSide/src/main.ts
+++ serverSide/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug'], //, 'verbose'
   });
-  await app.listen(3000);
+  // "start:debug": "NODE_ENV=develop nest start --debug --watch"
+  // といった具合にdevelopやproductionを切り替えることができる。
+  // 今回はここは主眼じゃないので、portの切り替え以外はやらない。
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap().then();
