303766554ff4fc04fcf505e8f7db7948ff13b542
ampersand <ampersand.dev@gmail.com>
2021-07-29 00:33:32 +0900

    参加者を取るるのではなく、参加者のidを取るようにした

--- serverSide/src/test/usecase/pair/addParticipanInPairtUsecase.spec.ts
+++ serverSide/src/test/usecase/pair/addParticipanInPairtUsecase.spec.ts
@@ -3,13 +3,20 @@ import { DividePairDomainService } from '../../../domain/pair/domainService/divi
 import { DisallowDuplicateParticipantInTPairDomainService } from '../../../domain/pair/domainService/disallowDuplicateParticipantDomainService';
 import { AddParticipantInPairUsecase } from '../../../usecase/pair/addParticipantInPairUsecase';
 import { dummyPair1, dummyPair2, dummyPair3 } from '../../../testUtil/dummy/dummyPair';
-import { dummyParticipant5 } from '../../../testUtil/dummy/dummyPerticipant';
+import { dummyParticipant3, dummyParticipant5 } from '../../../testUtil/dummy/dummyPerticipant';
+import { InMemoryParticipantRepository } from '../../../infra/db/inMemory/inMemoryParticipantRepository';
 
 describe('AddParticipantInPairUsecase', (): void => {
-  const repo = new InMemoryPairRepository();
-  const addService = new DisallowDuplicateParticipantInTPairDomainService(repo);
-  const divideService = new DividePairDomainService(repo);
-  const usecase = new AddParticipantInPairUsecase(repo, addService, divideService);
+  const participantRepository = new InMemoryParticipantRepository();
+  const pairRepository = new InMemoryPairRepository();
+  const addService = new DisallowDuplicateParticipantInTPairDomainService(pairRepository);
+  const divideService = new DividePairDomainService(pairRepository);
+  const usecase = new AddParticipantInPairUsecase(
+    participantRepository,
+    pairRepository,
+    addService,
+    divideService,
+  );
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -24,12 +31,19 @@ describe('AddParticipantInPairUsecase', (): void => {
       const spy2 = jest
         .spyOn(InMemoryPairRepository.prototype, 'update')
         .mockResolvedValueOnce(dummyPair1);
-      const data = { pairName: 'a', addParticipant: dummyParticipant5 };
+      const spy3 = jest
+        .spyOn(InMemoryParticipantRepository.prototype, 'findOne')
+        .mockResolvedValueOnce(dummyParticipant3);
+      const data = {
+        pairId: dummyPair1.id.toValue(),
+        addParticipantId: dummyParticipant5.id.toValue(),
+      };
       // 結果確認
       const result = await usecase.do(data);
       await expect(result.length).toStrictEqual(1);
       expect(spy).toHaveBeenCalledTimes(1);
       expect(spy2).toHaveBeenCalledTimes(1);
+      expect(spy3).toHaveBeenCalledTimes(1);
     });
     test('[正常]3名のペアに1名増えてペアが2つに分割される', async () => {
       // データ作成
@@ -37,12 +51,19 @@ describe('AddParticipantInPairUsecase', (): void => {
         .spyOn(InMemoryPairRepository.prototype, 'findOne')
         .mockResolvedValueOnce(dummyPair3);
       const spy2 = jest.spyOn(divideService, 'do').mockResolvedValueOnce([dummyPair1, dummyPair2]);
-      const data = { pairName: 'a', addParticipant: dummyParticipant5 };
+      const spy3 = jest
+        .spyOn(InMemoryParticipantRepository.prototype, 'findOne')
+        .mockResolvedValueOnce(dummyPair3.participants[0]);
+      const data = {
+        pairId: dummyPair1.id.toValue(),
+        addParticipantId: dummyParticipant5.id.toValue(),
+      };
       // 結果確認
       const result = await usecase.do(data);
       await expect(result.length).toStrictEqual(2);
       expect(spy).toHaveBeenCalledTimes(1);
       expect(spy2).toHaveBeenCalledTimes(1);
+      expect(spy3).toHaveBeenCalledTimes(1);
     });
   });
 });
