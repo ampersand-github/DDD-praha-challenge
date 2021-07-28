b962f988a22d222a4fb7b76b582b3ffe6d9f4019
ampersand <ampersand.dev@gmail.com>
2021-07-29 00:29:19 +0900

    pairNameは誤り、pairIdが正しい

--- serverSide/src/domain/pair/repositoryInterface/IPairRepository.ts
+++ serverSide/src/domain/pair/repositoryInterface/IPairRepository.ts
@@ -3,7 +3,7 @@ import { Participant } from '../../participant/participant';
 
 export interface IPairRepository {
   findAll(): Promise<Pair[]>;
-  findOne(pairName: string): Promise<Pair>;
+  findOne(pairId: string): Promise<Pair>;
   create(pair: Pair): Promise<Pair>;
   update(pair: Pair): Promise<Pair>;
   delete(pair: Pair): Promise<void>;
